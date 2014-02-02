fc666cb8aeac629d99b5930f05eb71d2ff0562cb
spirilis <spirilis@spirilis.net>
2014-02-02 11:31:30 -0500

    Modified TM4C129x example to reflect the new hw_inc.h usage.

--- example_tm4c1294ncpdt.c
+++ example_tm4c1294ncpdt.c
@@ -1,18 +1,7 @@
 /* newlp1202.c
  */
 
-// needed for driverlib rom stuff
-#define TARGET_IS_SNOWFLAKE_RA0 1
-
-#include <stdint.h>
-#include <stdbool.h>
-#include "inc/tm4c1294ncpdt.h"
-#include "inc/hw_types.h"
-#include "inc/hw_memmap.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/rom.h"
-#include "driverlib/rom_map.h"
-#include "driverlib/pin_map.h"
+#include "hw_inc.h"
 #include "driverlib/gpio.h"
 #include "driverlib/ssi.h"
 
