55f78df35a66224b373b2dbe833161d68b9071f5
spirilis <spirilis@spirilis.net>
2014-03-17 16:25:48 -0400

    Added details about the Tiva Connected LaunchPad

---
+++ hw_inc_tm4c1294ncpdt.h
@@ -0,0 +1,12 @@
+// needed for driverlib rom stuff
+#define TARGET_IS_TM4C129_RA1 1
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c1294ncpdt.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/pin_map.h"
